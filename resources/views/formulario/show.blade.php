3da3ae7c921686e62f41d69a41234d57b8805ed4
Javier <jcarrillo616@gmail.com>
2026-03-02 14:51:39 -0400

    cambio de bombre

--- resources/views/formulario/show.blade.php
+++ resources/views/formulario/show.blade.php
@@ -612,7 +612,7 @@
                     </div>
                     <div style="padding: 15px;">
                         <div class="data-display {{ empty($formulario->autorizado_nombre_2) ? 'empty' : '' }}" style="border: none; border-bottom: 1px solid #000; background: transparent; font-size: 0.7rem;">
-                            {{ $formulario->autorizado_nombre_2 ?? 'PAOLA YÉPEZ-ALCALÁ' }}
+                            {{ $formulario->autorizado_nombre_2 ?? 'LIC. CAROLINA GÓMEZ' }}
                         </div>
                     </div>
                     <div style="padding: 15px; border-left: 1px solid #000;">
